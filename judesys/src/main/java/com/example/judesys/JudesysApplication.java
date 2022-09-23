0badb690b3c5c17266559c4f7d19a3f44e7cfa38
Vladislav Ryžovas <vladislav.ryzovas@gmail.com>
2022-09-23 16:45:00 +0300

    Initial commit

---
+++ judesys/src/main/java/com/example/judesys/JudesysApplication.java
@@ -0,0 +1,13 @@
+package com.example.judesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudesysApplication.class, args);
+	}
+
+}
